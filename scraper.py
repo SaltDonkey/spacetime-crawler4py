3bfd7189004cc344789797039f81fc41ebada97b
Kevin <wukevin6@gmail.com>
2023-04-28 17:24:05 -0700

    Adding odc and ova to blacklisted extensions

--- scraper.py
+++ scraper.py
@@ -112,7 +112,7 @@ def is_valid(url):
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz|ppsx|class)$", parsed.path.lower()):
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz|ppsx|class|odc|ova)$", parsed.path.lower()):
             return False
         
         # The regex string will account for all URLs in this form:
