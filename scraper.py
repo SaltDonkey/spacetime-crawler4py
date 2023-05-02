983defd22e77fb4105078c4e12eee24522cc3f92
MichaelKND <dinhm1@uci.edu>
2023-05-01 22:08:00 -0700

    Added extra file extensions and updated trap navigator

--- scraper.py
+++ scraper.py
@@ -111,7 +111,7 @@ def is_valid(url):
             + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
-            + r"|thmx|mso|arff|rtf|jar|csv|odc|ova"
+            + r"|thmx|mso|arff|rtf|jar|csv|odc|ova|bib|r|txt|py|scm|rkt|java|ds_store|pd|sql|xml|war|c|h|sh|svg|fig|cfg|m"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz|ppsx|class)$", parsed.path.lower()):
             return False
         
